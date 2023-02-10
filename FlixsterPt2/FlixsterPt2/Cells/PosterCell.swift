d824a3cb3b868efeee8d2154bbab2c806760fb92
Camila Aichele <103790405+camila1014@users.noreply.github.com>
2023-02-10 10:34:16 -0500

    Add files via upload

---
+++ FlixsterPt2/FlixsterPt2/Cells/PosterCell.swift
@@ -0,0 +1,17 @@
+//
+//  PosterCell.swift
+//  FlixsterPt2
+//
+//  Created by Camila Aichele on 2/9/23.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+}
